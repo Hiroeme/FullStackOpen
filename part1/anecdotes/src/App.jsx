a982310ec05872c70a384e90f0117cd036037571
Hiroeme <83380500+Hiroeme@users.noreply.github.com>
2024-07-06 23:34:03 -0700

    added exercises 1.12 - 1.14

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+
+const Button = ({onClick, text}) => {
+ 
+  return (
+    <button onClick={onClick}>{text}</button>
+
+  )
+}
+
+const Header = ({text}) => <h1>{text}</h1>
+
+const Anecdote = ({points, anecdotes}) => {
+  if (points.reduce((partialSum, a) => partialSum + a, 0) === 0) {
+    return <div>No votes have been cast yet!</div>
+  } 
+
+  const indexofLargestValue = points.reduce(
+    (maxIndex, currentValue, currentIndex, array) => currentValue > array[maxIndex] ? currentIndex : maxIndex, 0)
+
+  return <div>
+    {anecdotes[indexofLargestValue]}
+    <p>has {points[indexofLargestValue]} votes</p>
+    </div>
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [points, updatePoints] = useState(new Uint8Array(anecdotes.length))
+
+  const randomSel = () => {
+    const num = Math.floor(Math.random() * anecdotes.length)
+    setSelected(num)
+  }
+
+  const incrementPt = () => {
+    const copy = [...points]
+    copy[selected] += 1
+    updatePoints(copy)
+  }
+
+  return (
+    <div>
+      <Header text='Anecdote of the Day'/>
+      <p>{anecdotes[selected]}</p>
+      <p>has {points[selected]} votes</p>
+      <Button onClick={incrementPt} text='vote'/>
+      <Button onClick={randomSel} text='next anecdote'/>
+
+      <Header text='Anecdote with the most Votes'/>
+      <Anecdote points={points} anecdotes={anecdotes}/>
+    </div>
+  )
+}
+
+export default App
